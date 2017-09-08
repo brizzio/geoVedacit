b1f43dc750d72cf995b29e78605713483010a1a6
altcom <fabrizio@altcom.net.br>
2017-09-08 03:31:25 -0300

    adicao de clientes e base anamanco, atualizacao do codigo js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
-    var map;
+    var gmap;
     var table;
+    var tabCliVedacit;
+    var errlocal = [];
+    var acertos = 0;
+    var erros = 0;
+    var pinBase ={};
     //cnpj	raiz	tipo	abertura	razao	logra	num	compl	bairro	cep	cod_ibge	uf	cidade	dddcel	
     //cel	dddcel2	cel2	dddcel3	cel3	dddfone	fone	dddfone2	fone2	dddfone3	fone3	dddfone4	
     //fone4	dddfone5	fone5	dddfax	fax	dddP	foneP	dddO	foneO	TC	cnae	cnae_descricao	cnae_s	
@@ -11,63 +16,56 @@
     
     function preload() {
       
-      table = loadTable("testetab.txt", "tsv", "header");
+      table = loadTable("anamanco1000.txt", "tsv", "header");
       tabCliVedacit = loadTable("clientesVedacit.txt", "tsv", "header");
       
       console.log('on preload');
+
+           
     }
 
     function setup() {
       //count the columns
       console.log('on setup');
-      //print(table.getRowCount() + " total rows in table");
-      //print(table.getColumnCount() + " total columns in table");
-      //print(table.getColumn("cnpj")[0]);
-          
-      //cycle through the table
-      // for (var r = 0; r < table.getRowCount(); r++)
-      //   for (var c = 0; c < table.getColumnCount(); c++) {
-      //     print(table.getString(r, c));
-      //   }
+      
+      var pinColor = "54e0ec";
+      var pinBase = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0,0),
+          new google.maps.Point(10, 34));
 
-      var map = new GMaps({
+      var gmap = new GMaps({
             div: '#map',
             lat: -15.77972,
             lng: -47.92972,
             zoom: 4
-        });
+          });   
+
+      var styles = {
+            default: null,
+            hide: [
+              {
+                featureType: 'poi',
+                stylers: [{visibility: 'off'}]
+              },
+              {
+                featureType: 'transit',
+                elementType: 'labels.icon',
+                stylers: [{visibility: 'off'}]
+              }
+            ]
+          };
+       //remove os indicadores padrao do google maps
+        gmap.setOptions({styles: styles['hide']});     
       
-        var styles = {
-          default: null,
-          hide: [
-            {
-              featureType: 'poi',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'labels.icon',
-              stylers: [{visibility: 'off'}]
-            }
-          ]
-        };  
-
-        var pinColor = "54e0ec";
-        var pinBase = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0,0),
-            new google.maps.Point(10, 34));
-
-      //remove os indicadores padrao do google maps
-      map.setOptions({styles: styles['hide']});
       //adiciona os marcadores de local para a base externa de clientes
         for (var r = 0; r < table.getRowCount(); r++) {
               var mapLatitude = table.getColumn("latitude")[r].replace(",", ".");
               var mapLongitude = table.getColumn("longitude")[r].replace(",", ".");
               var mapMarkTitle = table.getColumn("razao")[r];
-             
+             print('base-anamanco');
               if(mapLatitude && mapLongitude){ 
-                  map.addMarker({
+                  gmap.addMarker({
                     lat: mapLatitude,
                     lng: mapLongitude,
                     title: mapMarkTitle,
@@ -79,79 +77,26 @@
               }
         }  
         
-        //monta os maracdores dos clientes vedacit
-        //Razao	NomeCliente	Bairro	Cidade	UF	TipoLogradouro	Logradouro	Numero	CEP
-        for (var r = 0; r < tabCliVedacit.getRowCount(); r++) {
-          var endereco = tabCliVedacit.getColumn("TipoLogradouro")[r] + ' ' +
-                         tabCliVedacit.getColumn("Logradouro")[r] + ', ' +
-                         tabCliVedacit.getColumn("Numero")[r] + ' ' +
-                         tabCliVedacit.getColumn("Cidade")[r] + ' ' +
-                         tabCliVedacit.getColumn("UF")[r];
-
-          print(endereco);
-
-          
-          var mapMarkCliente = tabCliVedacit.getColumn("NomeCliente")[r];
-         
-           GMaps.geocode({
-              address: endereco,
-              callback: function(results, status) {
-             // alert(status);
-                if (status == 'OK') {
-                  var latlng = results[0].geometry.location;
-                  map.setCenter(latlng.lat(), latlng.lng());
-                                    
-                  map.addMarker({
-                    lat: latlng.lat(),
-                    lng: latlng.lng(),
-                    title: mapMarkCliente,
-                    click: function(e) {
-                      alert('Cliente na Base Vedacit - ' + e.title);
+        //monta os maracadores dos clientes vedacit
+        
+         //adiciona os marcadores de local para os clientes vedacit
+         //razao	cliente	bairro	cidade	uf	pre	logradouro	numero	cep	endereco	Latitude	Longitude	Endereco Formatado
+         for (var r = 0; r < tabCliVedacit.getRowCount(); r++) {
+                var mapLatitude = tabCliVedacit.getColumn("Latitude")[r].replace(",", ".");
+                var mapLongitude = tabCliVedacit.getColumn("Longitude")[r].replace(",", ".");
+                var mapMarkTitle = tabCliVedacit.getColumn("razao")[r];
+              print('clientes-vedacit');
+                    if(mapLatitude && mapLongitude){ 
+                        gmap.addMarker({
+                          lat: mapLatitude,
+                          lng: mapLongitude,
+                          title: mapMarkTitle,
+                          click: function(e) {
+                            alert('Cliente na Base Vedacit - ' + e.title);
+                          }
+                        });
                     }
-                  });
-                }else{
-                //alert('error');
-                }
-              }
-            });
-          }
+          }  
+
     }  
         
-
-
-
-   
-    
-
-
-    
-            
-            
-            
-              
-            
-            // GMaps.geocode({
-            //   address: "Alameda santos, 734 São paulo",
-            //   callback: function(results, status) {
-            //  // alert(status);
-            //     if (status == 'OK') {
-            //       var latlng = results[0].geometry.location;
-            //       map.setCenter(latlng.lat(), latlng.lng());
-            //       map.setOptions({styles: styles['hide']});
-                  
-            //       map.addMarker({
-            //         lat: latlng.lat(),
-            //         lng: latlng.lng(),
-            //         title: 'Office',
-            //         click: function(e) {
-            //           alert('You clicked in this marker');
-            //         }
-            //       });
-            //     }else{
-            //     alert('error');
-            //     }
-            //   }
-              
-            // });
-    //});
-  
